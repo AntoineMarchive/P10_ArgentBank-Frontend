49246f6bada9e1cd7365a779c51acf5c8e27f78b
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-09 13:39:58 +0200

    Error Page

---
+++ src/assets/pages/Error.jsx
@@ -0,0 +1,17 @@
+import { Layout } from "../components/layout.jsx";
+
+export function Error() {
+  return (
+    <main>
+      <Layout>
+        <div className="error">
+          <p className="error__number">404</p>
+          <p className="error__text">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <p className="error__link">Retourner sur la page d'accueil</p>
+        </div>
+      </Layout>
+    </main>
+  );
+}
