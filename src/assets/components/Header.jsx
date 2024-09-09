7fa914863386b12097d8ccbd792d0396585c88a9
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-09 13:37:09 +0200

    Header

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,21 @@
+export function Header() {
+    return (
+      <header className="main-nav">
+        <Navlink className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src="./img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Navlink>
+        <div>
+          <Navlink className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Navlink>
+        </div>
+      </header>
+    );
+  }
+  
