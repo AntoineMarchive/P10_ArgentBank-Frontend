4101e6678efef4890f67e02c948a9b09029de821
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-09 13:59:08 +0200

    header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
       <header className="main-nav">
-        <Navlink className="main-nav-logo" href="./index.html">
+        <Navlink className="main-nav-logo" to="./index.html">
           <img
             className="main-nav-logo-image"
             src="./img/argentBankLogo.png"
@@ -10,7 +10,7 @@ export function Header() {
           <h1 className="sr-only">Argent Bank</h1>
         </Navlink>
         <div>
-          <Navlink className="main-nav-item" href="./sign-in.html">
+          <Navlink className="main-nav-item" to="./sign-in.html">
             <i className="fa fa-user-circle"></i>
             Sign In
           </Navlink>
