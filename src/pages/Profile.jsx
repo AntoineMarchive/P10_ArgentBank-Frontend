5de995ad3335a57186dcc6752af6f3ba9b7fa431
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-10 08:59:38 +0200

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,31 +5,30 @@ import { Account } from "../components/Account.jsx";
 export function Profile() {
   return (
     <Layout>
-      <Account />
-      <Account />
-      <Account />
+     <main class="main bg-dark">
+      <div class="header">
+        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <button class="edit-button">Edit Name</button>
+      </div>
+
+        <h2 className="sr-only">Accounts</h2>
+        <Account
+          title="Argent Bank Checking (x8349)"
+          amount="$2,082.79"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Savings (x6712)"
+          amount="$10,928.42"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Credit Card (x8349)"
+          amount="$184.30"
+          description="Current Balance"
+        />
+      </main>
     </Layout>
   );
 }
 
-
-//Header deconnexion
-{/* <nav className="main-nav">
-  <a className="main-nav-logo" href="./index.html">
-    <img
-      className="main-nav-logo-image"
-      src="./img/argentBankLogo.png"
-      alt="Argent Bank Logo"
-    />
-    <h1 className="sr-only">Argent Bank</h1>
-  </a>
-  <div>
-    <a className="main-nav-item" href="./user.html">
-      <i className="fa fa-user-circle"></i>
-      Tony
-    </a>
-    <a className="main-nav-item" href="./index.html">
-      <i className="fa fa-sign-out"></i>
-      Sign Out
-    </a>
-  </div>
