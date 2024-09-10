881ebe9347319bd6c8c68fa4a714f511214cdede
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-10 08:52:35 +0200

    import in Home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Layout } from "../components/Layout.jsx";
-import { featureItem } from "../components/FeatureItem.jsx";
+import { FeatureItem } from "../components/FeatureItem.jsx";
+import '../../src/index.css';
+import iconChat from "../img/icon-chat.png";
+import iconMoney from "../img/icon-money.png";
+import iconSecurity from "../img/icon-security.png";
+
 
 export function Home() {
   return (
@@ -18,32 +23,22 @@ export function Home() {
           </section>
         </div>
         <section className="features">
-          <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src="image" alt="title" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src="image" alt="title" className="feature-icon" />
-            <h3 className="feature-item-title">
-              More savings means higher rates
-            </h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src="image" alt="title" className="feature-icon" />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
+        <h2 className="sr-only">Features</h2>
+          <FeatureItem 
+            imgSrc={iconChat}
+            title="You are our #1 priority"
+            description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+          />
+          <FeatureItem 
+            imgSrc={iconMoney}
+            title="More savings means higher rates"
+            description="The more you save with us, the higher your interest rate will be!"
+          />
+          <FeatureItem 
+            imgSrc={iconSecurity}
+            title="Security you can trust"
+            description="We use top of the line encryption to make sure your data and money is always safe."
+          />
         </section>
       </main>
     </Layout>
