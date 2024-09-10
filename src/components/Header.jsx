d52c18874fe2d493891e21c9bc08ba26bbb7e7f5
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-10 09:17:36 +0200

    header modif "to"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
+import "../index.css";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
-    return (
-      <header className="main-nav">
-        <Navlink className="main-nav-logo" to="./index.html">
-          <img
-            className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
+  return (
+    <header className="main-nav">
+      <Navlink className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src="./img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Navlink>
+      <div>
+        <Navlink className="main-nav-item" to="/SignIn">
+          <i className="fa fa-user-circle"></i>
+          Sign In
         </Navlink>
-        <div>
-          <Navlink className="main-nav-item" to="./sign-in.html">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </Navlink>
-        </div>
-      </header>
-    );
-  }
-  
+      </div>
+    </header>
+  );
+}
