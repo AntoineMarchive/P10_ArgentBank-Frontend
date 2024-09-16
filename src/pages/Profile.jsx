f05300167a82aefe3ed76c7b6b9f0a287539ae11
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-16 11:21:19 +0200

    profile edition

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Layout } from "../components/Layout.jsx";
 import { Account } from "../components/Account.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserData, toggleEditForm, updateUserData } from "../redux/features/profile/profile.slice.js";
+import { useNavigate } from "react-router-dom";
 
 export function Profile() {
+  const dispatch = useDispatch();
+  const { firstName, lastName, error, isEditing, loading } = useSelector((state) => state.profile);
+  const token = useSelector((state) => state.auth.token);
+  const navigate = useNavigate();
+
+  const [newFirstName, setNewFirstName] = useState(firstName);
+  const [newLastName, setNewLastName] = useState(lastName);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchUserData(token));
+    } else if (!token || error) {
+      navigate("/Signin");
+    }
+  }, [token, navigate, dispatch, error]);
+
+  useEffect(() => {
+    setNewFirstName(firstName);
+    setNewLastName(lastName);
+  }, [firstName, lastName]);
+
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(updateUserData({ token, firstName: newFirstName, lastName: newLastName }));
+    dispatch(toggleEditForm());
+  };
+
+  const editMode = () => {
+    dispatch(toggleEditForm());
+  };
+
   return (
     <Layout>
-     <main class="main bg-dark">
-      <div class="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
-        <button class="edit-button">Edit Name</button>
-      </div>
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>
+            Welcome back
+            <br />
+            {firstName} {lastName}!
+          </h1>
+          {!isEditing ? (
+            <button onClick={editMode} className="edit-button">
+              Edit Name
+            </button>
+          ) : (
+            <form onSubmit={submit} className="edit-form">
+              <div className="inputs-wrapper">
+                <input
+                  type="text"
+                  value={newFirstName}
+                  onChange={(e) => setNewFirstName(e.target.value)}
+                  placeholder="First Name"
+                />
+                <input
+                  type="text"
+                  value={newLastName}
+                  onChange={(e) => setNewLastName(e.target.value)}
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="buttons-wrapper">
+                <button className="edit-button" type="submit">
+                  Save
+                </button>
+                <button onClick={editMode} className="edit-button" type="button">
+                  Cancel
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
 
         <h2 className="sr-only">Accounts</h2>
         <Account
@@ -31,4 +98,3 @@ export function Profile() {
     </Layout>
   );
 }
-
