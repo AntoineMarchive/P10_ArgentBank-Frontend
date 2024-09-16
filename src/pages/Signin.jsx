ac0aeecd59b4c9e6294d95c111b533c00a409a02
AntoineMarchive <antoine.marchive@orange.fr>
2024-09-16 11:22:15 +0200

    signin edition

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
 import { Layout } from "../components/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { loginAction } from "../redux/features/auth/auth.actions.js";
 
 export function SignIn() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { errorMessage, isLoading, token } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (token) {
+      navigate("/profile");
+    }
+  }, [token, navigate]); // Ajout de navigate comme dépendance
+
+  const submit = (e) => {
+    e.preventDefault();
+    const fd = new FormData(e.target);
+
+    dispatch(
+      loginAction({
+        email: fd.get("email"),
+        password: fd.get("password"),
+      })
+    );
+  };
+
   return (
     <div>
       <Layout>
@@ -9,30 +35,32 @@ export function SignIn() {
           <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign In</h1>
-            <form>
+            <form onSubmit={submit}>
               <div className="input-wrapper">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input name="email" type="text" id="username" />
               </div>
 
               <div className="input-wrapper">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input name="password" type="password" id="password" />
               </div>
 
               <div className="input-remember">
                 <input type="checkbox" id="remember-me" />
                 <label htmlFor="remember-me">Remember me</label>
               </div>
-              
-              {/* PLACEHOLDER DUE TO STATIC SITE */}
-              <a href="./user.html" className="sign-in-button">
-                Sign In
-              </a>
-              {/* SHOULD BE THE BUTTON BELOW */}
-              {/* <button className="sign-in-button">Sign In</button> */}
-              {/*  */}
+
+              <input
+                type="submit"
+                className="sign-in-button"
+                value={isLoading ? "Loading..." : "Sign In"} // Indicateur de chargement
+                disabled={isLoading} // Désactiver le bouton lors du chargement
+              />
             </form>
+
+            {/* Affichage conditionnel du message d'erreur */}
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
           </section>
         </main>
       </Layout>
